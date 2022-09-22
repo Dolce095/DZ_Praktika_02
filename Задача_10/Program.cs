9b1b75e5b762243074262042c5bfc931e6c235d5
Alexander Efremov <dolce095.ae@gmail.com>
2022-09-22 16:49:14 +0300

    DZ_2

---
+++ Задача_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа. 456 -> 5
+
+Console.Clear();
+Console.Write("Введите число, ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 1000)
+{
+    Console.Write("Ты издеваешься, введите заново");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine((n / 10) % 10);
---
+++ Задача_15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным. 6 -> да; 7 -> да; 1 -> нет
+
+Console.Clear();
+Console.Write("Введите число, обозначающее день недели,  ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 5 && n >= 1)
+{
+    Console.Write("Нет");
+int n1 = Convert.ToInt32(Console.ReadLine());
+}
+else 
+{
+Console.Write("Да");
+}
